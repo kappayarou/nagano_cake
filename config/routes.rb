f1c3be4baf73b936b01bd6865bdf4a6aa4694a9a
kappayarou <koyomi.vanpire@gmail.com>
2021-10-09 14:45:42 +0000

    [Add] genre_index,genre_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
-  resources :genres, only:[:index, :create, :edit, :update]
+
+    namespace :admin do
+    resources :genres, only:[:index, :create, :edit, :update]
+  end
 end
